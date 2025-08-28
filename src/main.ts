ae9fe4c2bcf015f30a5acf86e72b4be0ffc005a5
Vasa <mail@vasa.me>
2025-08-28 11:24:21 +0200

    chore: enhance Sentry initialization and update workspace configuration

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ import { useSyncUser } from "@/composables/useSyncUser.ts";
 
 const app = createApp(App);
 
-if (import.meta.env.MODE === "production") {
+if (import.meta.env.MODE === "production" && import.meta.env.VITE_SENTRY_DSN) {
   Sentry.init({
     app,
-    dsn: import.meta.env.VITE_SENTRY_DSN as string,
+    dsn: import.meta.env.VITE_SENTRY_DSN,
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
